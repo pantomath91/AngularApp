4eee6e3aaa69cf5df75ceee433a3478e6fdf038b
schnlohani <schnlohani@gmail.com>
2018-08-18 12:32:14 +0530

    adding some files to project

---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {Ngmodule, Component} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
